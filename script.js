efd825964a2355c720e05f7e36dc5732f5261455
Muhammad Rauf <abd.rauf.lamada@gmail.com>
2022-01-28 14:00:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function sumHarga(){
+  let liter = 0;
+  let premium = 6450;
+  let pertalite = 7650;
+
+  for(let i = 1; i <= 20; i++){
+    const countLiter = liter[i]++
+    const countPremium = premium[i] + premium
+    const countPertalite = pertalite[i] + pertalite 
+
+    return countLiter, countPremium, countPertalite
+  }
+}
